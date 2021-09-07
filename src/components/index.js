f97e162596b00121df4f005faa24674a89e51302
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-09-07 14:30:03 +0530

    Feat: Search added

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import MobileHeader from "./MobileHeader/MobileHeader"
 import AddPlaylist from "./AddPlaylist/AddPlaylist"
 import PlaylistList from "./PlaylistList/PlaylistList"
 import VideoPlayer from "./VideoPlayer/VideoPlayer"
+import SearchBar from "./SearchBar/SearchBar"
 
-export {VideoList, VideoOptions, MobileHeader, AddPlaylist, PlaylistList, VideoPlayer}
+export {VideoList, VideoOptions, MobileHeader, AddPlaylist, PlaylistList, VideoPlayer, SearchBar}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
 import '../stylesheets/home-style.css';
 import {useVideo} from "../videoContext";
 import { CommonLoading } from 'react-loadingg';
-import { VideoList } from "../components";
+import { SearchBar, VideoList } from "../components";
 
 export function Home(){
     const {dispatchHistory,videosDB} = useVideo();
     return(
+        <>
+        <SearchBar/>
         <div className="video-wrapper">
             {!videosDB && <CommonLoading color="yellow" size="large"/>}
             {videosDB && videosDB.length === 0 && <CommonLoading color="yellow" size="large"/>}
             {videosDB && videosDB.map(video=>{
-                return <div onClick={()=>dispatchHistory({type:"addToHistory",payload:video})}>
-                    <VideoList key={video._id} video={video}/>
+                return <div key={video._id} onClick={()=>dispatchHistory({type:"addToHistory",payload:video})}>
+                    <VideoList video={video}/>
                 </div>
             })}
         </div>
+        </>
     )
 }
