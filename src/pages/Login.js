4ff67170745eb0454ed76a4f8ccd09a2225ce3ce
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-09-29 14:43:47 +0530

    Feat: Login with demo credentials added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,16 +18,27 @@ export default function Login(){
         login?logoutHandler():loginHandler(username,password)
     }
 
+    function authHandlerWithDemo(){
+        if(!login){
+            setLoader(true)
+            setTimeout(()=>{
+                setLoader(false)
+            },5000)
+        }
+        login?logoutHandler():loginHandler(username="Demo",password="Demo")
+    }
+
     return <>
     <div className="login">
         <div className="login-card">
             <div className="login-title">Login</div>
             { !login && <><div className="login-label">Username</div>
-            <input className="login-input" onChange={e => username=e.target.value} type="text"></input>
+            <input className="login-input" value={username} onChange={e => username=e.target.value} type="text"></input>
             <div className="login-label">Password</div>
-            <input className="login-input" onChange={e => password=e.target.value} type="password"></input></>}
+            <input className="login-input" value={password} onChange={e => password=e.target.value} type="password"></input></>}
             {login && <div className="login-label">You're already logged in.</div>}
             <div><button className="login-button" onClick={authHandler}>{login?"Logout":"Login"}</button></div>
+            {!login && <div><button className="login-button" onClick={authHandlerWithDemo}>Login with Demo credentials</button></div>}
             {loader && <SemipolarLoading color="yellow" size="large"/>}
         </div>
     </div>
